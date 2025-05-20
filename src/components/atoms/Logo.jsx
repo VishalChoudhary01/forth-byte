132e05b06b60db712bf656937e40a9b3a2a4a476
Vishal <vishal.bhumca21@gmail.com>
2025-05-20 19:02:12 +0530

    menu item lgtext1.3rem

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className='flex items-center md:gap-x-2 '>
         <img src={forthbytelogo} alt="forth byte Logo"  className='rounded-full'/>
-        <p className='font-Neonderthaw lg:text-[1.5rem] text-[1.2rem] font-medium'>Forth Byte</p>
+        <p className='font-Neonderthaw lg:text-[1.4rem] text-[1.2rem] font-medium'>Forth Byte</p>
     </div>
   )
 }
