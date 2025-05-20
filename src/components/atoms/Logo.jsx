08df6283a57772ede4e7c5d56ad44d912516a9b0
Vishal <vishal.bhumca21@gmail.com>
2025-05-20 21:13:58 +0530

    navber desktop completed

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className='flex items-center md:gap-x-2 '>
         <img src={forthbytelogo} alt="forth byte Logo"  className='rounded-full'/>
-        <p className='font-Neonderthaw lg:text-[1.4rem] text-[1.2rem] font-medium'>Forth Byte</p>
+        <p className='font-RobotoMono lg:text-[1.4rem] text-[1.2rem] font-semibold'>Forth Byte</p>
     </div>
   )
 }
--- src/components/moleclues/Navbar.jsx
+++ src/components/moleclues/Navbar.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
 import Logo from "./../atoms/Logo"
 import {navbarMenu} from "./../../constants/index"
 import { NavLink } from 'react-router'
 import Toggle from '../atoms/Toggle'
 const Navbar = () => {
   return (
-    <nav className='w-full lg:py-2.5 py-2 lg:text-[1.3rem] lg:px-8 md:px-5 px-3 bg-NavBg fixed z-40    font-medium dark:bg-NavDarkBg text-heading1 dark:text-dark-heading1 transition-all duration-400   flex items-center justify-between'>
+    <nav className='w-full lg:py-2.5 py-2 lg:text-[1.3rem] lg:px-8 md:px-5 px-3 bg-NavBg fixed z-40 backdrop-blur-md dark:bg-NavDarkBg text-heading1 dark:text-dark-heading1 transition-all duration-400   flex items-center justify-between'>
       <Logo/>
-      <ul className='md:flex hidden xl:gap-x-9 font-OpenSans lg:gap-x-8 md:gap-x-6'>
+      <ul className='md:flex hidden xl:gap-x-9  lg:gap-x-8 md:gap-x-6'>
         {
           navbarMenu.map((menu,indx)=>(
             <NavLink 
               to={menu.path}
               key={indx}
               className={({ isActive }) => 
-                `hover:text-red-500 transition-colors ${
-                  isActive 
-                    ? 'text-red-600 dark:text-red-400 font-semibold' 
-                    : 'text-black dark:text-gray-300'
-                }`
-              }
+  `relative pb-1 transition-all duration-300 font-Raleway hover:scale-105
+   hover:text-link-hover hover:dark:text-dark-link-hover
+   ${
+     isActive 
+       ? 'text-link font-[500] dark:text-dark-link after:w-full after:bg-link dark:after:bg-dark-link after:animate-underlineExpand'
+       : 'after:w-0 after:bg-transparent'
+   }
+   after:absolute after:left-0 after:bottom-0 after:h-[2px] after:transition-all`
+}
             >
               {menu.name}
             </NavLink>
