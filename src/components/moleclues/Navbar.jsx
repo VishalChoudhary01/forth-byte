c9f1376dd82b2a48fd206dba625529824f28b9bf
Vishal <vishal.bhumca21@gmail.com>
2025-05-20 18:58:36 +0530

    py2.5 nabar

--- src/components/moleclues/Navbar.jsx
+++ src/components/moleclues/Navbar.jsx
@@ -5,13 +5,21 @@ import { NavLink } from 'react-router'
 import Toggle from '../atoms/Toggle'
 const Navbar = () => {
   return (
-    <nav className='w-full lg:py-1.5 py-2 lg:text-[1.1rem] lg:px-8 md:px-5 px-3 bg-NavBg fixed z-40    font-medium dark:bg-NavDarkBg text-heading1 dark:text-dark-heading1 transition-all duration-400   flex items-center justify-between'>
+    <nav className='w-full lg:py-2.5 py-2 lg:text-[1.1rem] lg:px-8 md:px-5 px-3 bg-NavBg fixed z-40    font-medium dark:bg-NavDarkBg text-heading1 dark:text-dark-heading1 transition-all duration-400   flex items-center justify-between'>
       <Logo/>
       <ul className='md:flex hidden xl:gap-x-9 font-OpenSans lg:gap-x-8 md:gap-x-6'>
         {
           navbarMenu.map((menu,indx)=>(
-            <NavLink key={indx} to={menu.path}
-            className={`${({isActive})=>isActive?"text-red-600":"text-black"}`}
+            <NavLink 
+              to={menu.path}
+              key={indx}
+              className={({ isActive }) => 
+                `hover:text-red-500 transition-colors ${
+                  isActive 
+                    ? 'text-red-600 dark:text-red-400 font-semibold' 
+                    : 'text-black dark:text-gray-300'
+                }`
+              }
             >
               {menu.name}
             </NavLink>
